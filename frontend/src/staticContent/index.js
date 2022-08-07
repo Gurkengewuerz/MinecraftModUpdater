aba9b098431c3568eeedd2cd70c33cc971e441cf
Gurkengewuerz <niklas@schuetrumpf.net>
2022-08-07 21:59:11 +0200

    chore: bump version to 1.0.2

--- frontend/src/staticContent/index.js
+++ frontend/src/staticContent/index.js
@@ -1,7 +1,7 @@
 export default {
   title: "Mod Updater",
   company: "TheTown",
-  version: "1.0.1",
+  version: "1.0.2",
   api: "https://mc8051.de/minecraft/updater",
   links: {
     github: "https://github.com/Gurkengewuerz/MinecraftModUpdater",
